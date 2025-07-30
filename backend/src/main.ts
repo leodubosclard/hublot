15a87d5f60c8af8cc9b80b21854bac0c185ab653
Léo Dubosclard <leo.dubosclard@epitech.eu>
2025-07-30 19:43:20 +0200

    initial commit

---
+++ backend/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3254;
+
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  });
+
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
+}
+
+bootstrap();
---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import { Center, Heading } from "@chakra-ui/react"
+
+export const App = () => {
+    return (
+        <Center h="100vh" w="100vw">
+            <Heading>Hublot</Heading>
+        </Center>
+    )
+}
