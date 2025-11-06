cfe36e134496a3f72b08c437a759e0df3bc8e904
Léo Dubosclard <leo.dubosclard@epitech.eu>
2025-11-06 07:11:43 +0100

    chore: clean code

--- frontend/src/components/Dashboard/Layout.tsx
+++ frontend/src/components/Dashboard/Layout.tsx
@@ -16,52 +16,50 @@ export type DashboardLayoutProps = {
     showInformations?: boolean;
 };
 
-export const DashboardLayout: FC<PropsWithChildren<DashboardLayoutProps>> = ({ menu, informations, logs, showLogs = true, showInformations = true, children }) => {
-    return (
-        <>
-            <HStack py={1} borderBottomWidth={1}>
-                {menu.map((item, index) => <DashboardMenu key={index} name={item.name} items={item.items} />)}
-            </HStack>
-            <Flex direction="row" width="100%" maxH="calc(100vh - 49px)">
-                <Box flex="3" h="calc(100vh - 49px)">
-                    <Flex direction="column" h="100%" w="100%">
-                        <Box flex="3" p={2}>{children}</Box>
-                        {showLogs && (
-                            <Box flex="1" p={2} bgColor="#FBFBFB" borderTopWidth={1} h="200px">
-                                <Text fontWeight="bold" pb={1}>Logs</Text>
-                                <VStack align="left" gap={0} pb={2} fontSize="14px" overflowY="auto" h="calc(100% - 24px)">
-                                    {logs.map((log, index) => (
-                                        <Text key={`log-${index}`} pl={2}>{log}</Text>
-                                    ))}
-                                </VStack>
+export const DashboardLayout: FC<PropsWithChildren<DashboardLayoutProps>> = ({ menu, informations, logs, showLogs = true, showInformations = true, children }) => (
+    <>
+        <HStack py={1} borderBottomWidth={1}>
+            {menu.map((item, index) => <DashboardMenu key={index} name={item.name} items={item.items} />)}
+        </HStack>
+        <Flex direction="row" width="100%" maxH="calc(100vh - 49px)">
+            <Box flex="3" h="calc(100vh - 49px)">
+                <Flex direction="column" h="100%" w="100%">
+                    <Box flex="3" p={2}>{children}</Box>
+                    {showLogs && (
+                        <Box flex="1" p={2} bgColor="#FBFBFB" borderTopWidth={1} h="200px">
+                            <Text fontWeight="bold" pb={1}>Logs</Text>
+                            <VStack align="left" gap={0} pb={2} fontSize="14px" overflowY="auto" h="calc(100% - 24px)">
+                                {logs.map((log, index) => (
+                                    <Text key={`log-${index}`} pl={2}>{log}</Text>
+                                ))}
+                            </VStack>
+                        </Box>
+                    )}
+                </Flex>
+            </Box>
+            {showInformations && (
+                <Box flex="1" p={4} borderLeftWidth={1} overflowY="scroll">
+                    <Text fontWeight="bold">Simulation:</Text>
+                    <UnorderedList>
+                        {informations.map((info, index) => (
+                            <Box key={`${info.name}-${index}`}>
+                                {Array.isArray(info.value) ? (
+                                    <ListItem>
+                                        {info.name}: <b>{info.value.length}</b>
+                                        <UnorderedList key={`${info.name}-${index}`}>
+                                            {info.value.map((value, index) => (
+                                                <ListItem key={`${info.name}-${index}-${value}`}>{value}</ListItem>
+                                            ))}
+                                        </UnorderedList>
+                                    </ListItem>
+                                ) : (
+                                    <ListItem key={`${info.name}-${index}`}>{info.name}: <b>{info.value ?? 'N/A'}</b></ListItem>
+                                )}
                             </Box>
-                        )}
-                    </Flex>
+                        ))}
+                    </UnorderedList>
                 </Box>
-                {showInformations && (
-                    <Box flex="1" p={4} borderLeftWidth={1} overflowY="scroll">
-                        <Text fontWeight="bold">Simulation:</Text>
-                        <UnorderedList>
-                            {informations.map((info, index) => (
-                                <Box key={`${info.name}-${index}`}>
-                                    {Array.isArray(info.value) ? (
-                                        <ListItem>
-                                            {info.name}: <b>{info.value.length}</b>
-                                            <UnorderedList key={`${info.name}-${index}`}>
-                                                {info.value.map((value, index) => (
-                                                    <ListItem key={`${info.name}-${index}-${value}`}>{value}</ListItem>
-                                                ))}
-                                            </UnorderedList>
-                                        </ListItem>
-                                    ) : (
-                                        <ListItem key={`${info.name}-${index}`}>{info.name}: <b>{info.value ?? 'N/A'}</b></ListItem>
-                                    )}
-                                </Box>
-                            ))}
-                        </UnorderedList>
-                    </Box>
-                )}
-            </Flex>
-        </>
-    );
-};
+            )}
+        </Flex>
+    </>
+);
