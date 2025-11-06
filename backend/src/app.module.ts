f2521a309c0a13e3c216fc5720a402961e0838d7
Léo Dubosclard <leo.dubosclard@epitech.eu>
2025-11-06 07:46:37 +0100

    feat: cron

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ConfigModule } from '@nestjs/config';
+import { CronModule } from 'src/cron/cron.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), CronModule],
   controllers: [AppController],
 })
 export class AppModule {}
