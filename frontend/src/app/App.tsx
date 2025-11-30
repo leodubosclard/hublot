9a29b9c3380f877eddd6dec14703edf84018779f
Léo Dubosclard <leo.dubosclard@epitech.eu>
2025-11-30 19:23:00 +0100

    feat: analytics

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,9 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { Analytics } from '@vercel/analytics/react';
 import { Router } from './Router';
 import theme from './theme';
 
 export const  App = () => (
     <ChakraProvider theme={theme}>
+      <Analytics />
       <Router />
     </ChakraProvider>
 );
